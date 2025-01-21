1abf579a3c70f892c10dec01c812c5b295c21953
harry843 <kelleherharry@gmail.com>
2025-01-21 14:24:46 +0000

    add meta tags to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,26 @@
 	import Feature from '../components/Hero/Feature.svelte';
 	import Hero from '../components/Hero/Hero/Hero.svelte';
 	import InvoiceDemo from '../components/Hero/InvoiceDemo/InvoiceDemo.svelte';
+	import { page } from '$app/stores';
+
 
 	let screenwidth: number = 0;
 </script>
+
+<svelte:head>
+	<title>Harry Kelleher</title>
+	<meta property="og:title" content="Invoice-r" />
+	<meta property="og:type" content="article" />
+	<meta name="author" content="Harry Kelleher" />
+	<meta name="article:published_time" content={new Date().toLocaleDateString()} />
+	<meta property="og:image" content="https://ibb.co/qD7WYwk" />
+	<meta
+		property="og:description"
+		content="Create your invoice in seconds. No sign-ups, no strings attached - absolutely free."
+	/>
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:locale" content="en_GB" />
+</svelte:head>
 
 <svelte:window bind:innerWidth={screenwidth} />
 
