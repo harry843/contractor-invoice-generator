d8e82b62fe1031a5cc85722f9a27985d868392de
harry843 <kelleherharry@gmail.com>
2025-01-22 09:28:51 +0000

    add clarity and ga to layout

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,6 +4,7 @@ export const load = async ({ url }) => {
             title: "Invoice-r",
             description: "Create your invoice in seconds. No sign-ups, no strings attached - absolutely free.",
             image: "https://i.ibb.co/crK4tmF/invoice-r-home.png",
+            author: "Harry Kelleher",
             url: url.href,
             type: "website",
             locale: "en_GB",
